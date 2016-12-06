fbe038619d861958413328b5f36f95aaf6418be5
Alex Knipfer <alexanderknipfer@gmail.com>
2016-12-06 16:22:39 -0600

    added food

--- src/sketch.js
+++ src/sketch.js
@@ -1,15 +1,28 @@
 var snake;
 var scl = 20;
+var food;
 
 function setup() {
     createCanvas(600, 600);
     snake = new Snake();
+    frameRate(10);
+    food = createVector(random(width), random(height));
+}
+
+function pickLocation(){
+    var cols = floor(width / scl);
+    var rows = floor(height / scl);
+    food = createVector(floor(random(cols)), floor(random(rows)));
+    food.mult(scl);
 }
 
 function draw() {
   background(51);
   snake.update();
   snake.show();
+
+  fill(255, 0, 100);
+  rect(food.x, food.y, scl, scl);
 }
 
 function keyPressed(){
